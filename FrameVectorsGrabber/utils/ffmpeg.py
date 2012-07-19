5f57a0fbb4c80bffc349f79721c044aa2d0cbd05
Luca Ferrari <luca.ferrari@risingbits.com>
2012-07-19 12:08:58 +0200

    New method to FFMpegWrapper to generate video from frames

--- FrameVectorsGrabber/utils/ffmpeg.py
+++ FrameVectorsGrabber/utils/ffmpeg.py
@@ -3,6 +3,8 @@ __author__ = 'luca'
 from PyQt4.QtCore import QProcess, QObject
 from PyQt4.QtGui import QApplication
 import os
+import time
+from utils.string import str_insert
 
 class FFMpegWrapper(object):
 
@@ -30,26 +32,26 @@ class FFMpegWrapper(object):
             process.finished.connect(_check_callback_status)
 
             #process.start("/usr/local/bin/ffmpeg", ['-i', input_file_path,'-t', '5', frames_output_path+'/Frame_%07d.'+frame_extension])
-            process.start("/usr/local/bin/ffmpeg", ['-i', input_file_path, '-r', '30', '-t', '3', '-s', '213x120',  '-ss', '00:01:30', frames_output_path+'/Frame_%07d.'+frame_extension])
+            #process.start("/usr/local/bin/ffmpeg", ['-i', input_file_path, '-r', '30', '-t', '3', '-s', '213x120',  '-ss', '00:01:30', frames_output_path+'/Frame_%07d.'+frame_extension])
+            process.start("/usr/local/bin/ffmpeg", ['-i', input_file_path, '-r', '15', '-t', '5', '-ss', '00:00:50', frames_output_path+'/Frame_%07d.'+frame_extension])
 
 
-            #process.startDetached("/usr/local/bin/ffmpeg", ['-i', input_file_path,'-t', '5', frames_output_path+'/Frame_%07d.'+frame_extension])
+    @classmethod
+    def generate_video(cls, input_frames_folder, file_name, frames_pattern= "Frame_%07d.png", finish_callbak = None, bitrate=1800, fps = 30):
 
-            #process.startDetached("/usr/local/bin/ffmpeg", ['-i', input_file_path,'-t', '2', output_dir+'/video_name/Frame_%07d.'+frame_extension])
-            #process.startDetached("/usr/local/bin/ffmpeg", ['-h'])
+        input_frames_pattern = "%s/%s" % (input_frames_folder, frames_pattern)
+        video_name = str_insert(file_name, "_%f" % time.time(), file_name.rfind("."))
 
-           # process = subprocess.Popen(['/usr/local/bin/ffmpeg',  '-i', input_file_path,'-t 5', '-ss 00:01:30', output_dir+'/video_name/Frame_%07d.'+frame_extension], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            #process = subprocess.Popen(['/usr/local/bin/ffmpeg', '-h'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            #process = subprocess.Popen(['ls'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            #stdout, stderr = process.communicate()
-            #print stdout
-            #matches = re.search(r"Duration:\s{1}(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),", stdout, re.DOTALL).groupdict()
+        video_output_path = "/tmp/%s" %video_name
 
-            #print matches['hours']
-            #print matches['minutes']
-            #print matches['seconds']
+        #Execute FFMPEG
+        def _check_callback_status(code, code2):
+            print "Processo FFMpeg finito, chiamo la callback"
+            if finish_callbak:
+                finish_callbak(code==0, video_output_path)
 
-           # finish_callbak()
+        process = QProcess(QApplication.instance())
+        process.finished.connect(_check_callback_status)
 
-
+        process.start("/usr/local/bin/ffmpeg", ['-r', str(fps), '-b', str(bitrate), '-i', input_frames_pattern, video_output_path])
 
